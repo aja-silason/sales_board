60bb98f3714232b747956b9dacb7a071dde530a8
aja-silason <ananiasjaimeaugusto@gmail.com>
2025-05-01 23:11:04 +0100

    feat: add validate

--- src/app/domain/employee/dto/create-employee.dto.ts
+++ src/app/domain/employee/dto/create-employee.dto.ts
@@ -1,19 +1,28 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty, IsString } from "class-validator";
 
 export class CreateEmployeeDto {
+
+    @ApiProperty()
+    id?: string;
+
     @IsString()
     @IsNotEmpty()
+    @ApiProperty()
     firstName: string;
 
     @IsString()
     @IsNotEmpty()
+    @ApiProperty()
     lastName: string;
 
     @IsString()
     @IsNotEmpty()
+    @ApiProperty()
     dateOfBirth: Date;
 
     @IsString()
     @IsNotEmpty()
+    @ApiProperty()
     identificaion: string;
 }
--- src/app/domain/employee/entities/employee.entity.ts
+++ src/app/domain/employee/entities/employee.entity.ts
@@ -1,1 +1,35 @@
-export class EmployeeEntity {}
+import { BadRequestException } from "@nestjs/common";
+import { CreateEmployeeDto } from "../dto/create-employee.dto";
+
+export class EmployeeEntity {
+
+    constructor(private readonly props: CreateEmployeeDto){}
+
+    public static create(props: CreateEmployeeDto){
+
+        this.validate(props);
+
+        return new EmployeeEntity({
+            id: crypto.randomUUID(),
+            ...props
+        })
+
+    }
+
+    private static validate(props: CreateEmployeeDto){
+
+        const isValidate: Array<keyof CreateEmployeeDto> = ["firstName", "lastName", "identificaion", "dateOfBirth"];
+        for(const key of isValidate){
+            const value = props[key];
+            if(value == undefined || value == null){
+                throw new BadRequestException(`${key} must be provide`);
+            }
+        }
+
+    }
+
+    public get allProps(){
+        return this.props;
+    }
+
+}
