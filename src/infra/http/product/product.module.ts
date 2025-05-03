a0549ffbd98d8c8c70ced256d1b1356922da911f
aja-silason <ananiasjaimeaugusto@gmail.com>
2025-05-03 17:57:23 +0100

    feat: add module

--- src/infra/http/product/product.module.ts
+++ src/infra/http/product/product.module.ts
@@ -1,9 +1,32 @@
 import { Module } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { ProductController } from './product.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductModel } from 'src/app/domain/product/model/product.model';
+import { ProductProtocol } from 'src/app/domain/product/protocol/product.protocol';
+import { TypeORMProductRepository } from 'src/infra/repository/product/product.repository';
+import { CreateProductUsecase } from 'src/usecase/product/create';
+import { DeleteProductUsecase } from 'src/usecase/product/delete';
+import { ListProductUsecase } from 'src/usecase/product/list';
+import { UpdateProductUsecase } from 'src/usecase/product/update';
+import { FindOneProductUsecase } from 'src/usecase/product/findOne';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([ProductModel])
+  ],
   controllers: [ProductController],
-  providers: [ProductService],
+  providers: [
+    ProductService,
+    CreateProductUsecase,
+    DeleteProductUsecase,
+    ListProductUsecase,
+    UpdateProductUsecase,
+    FindOneProductUsecase,
+    {
+      provide: ProductProtocol,
+      useClass: TypeORMProductRepository
+    }
+  ],
 })
 export class ProductModule {}
