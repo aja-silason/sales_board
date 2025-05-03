99edc05051d934e7501fa6d424f830ab6ef1b0cf
aja-silason <ananiasjaimeaugusto@gmail.com>
2025-05-03 18:56:16 +0100

    feat: add service

--- src/infra/http/product/product.service.ts
+++ src/infra/http/product/product.service.ts
@@ -1,26 +1,40 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProductDto } from '../../../app/domain/product/dto/create-product.dto';
 import { UpdateProductDto } from '../../../app/domain/product/dto/update-product.dto';
+import { CreateProductUsecase } from 'src/usecase/product/create';
+import { ListProductUsecase } from 'src/usecase/product/list';
+import { UpdateProductUsecase } from 'src/usecase/product/update';
+import { DeleteProductUsecase } from 'src/usecase/product/delete';
+import { FindOneProductUsecase } from 'src/usecase/product/findOne';
 
 @Injectable()
 export class ProductService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+
+  constructor(
+    private readonly createProductUsecase: CreateProductUsecase,
+    private readonly listProductUsecase: ListProductUsecase,
+    private readonly findOneProductUsecase: FindOneProductUsecase,
+    private readonly updateProductUsecase: UpdateProductUsecase,
+    private readonly delteProductUsecase: DeleteProductUsecase
+  ){}
+
+  async create(body: CreateProductDto) {
+    return await this.createProductUsecase.execute(body);
   }
 
-  findAll() {
-    return `This action returns all product`;
+  async findAll() {
+    return await this.listProductUsecase.execute();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(id: string) {
+    return await this.findOneProductUsecase.execute(id);
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  async update(id: string, newBody: UpdateProductDto) {
+    return await this.updateProductUsecase.execute(id, newBody);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async remove(id: string) {
+    return await this.delteProductUsecase.execute(id);
   }
 }
